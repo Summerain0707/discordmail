4711a330f61a716d3f54dc7cb87951005c80fb63
Summerain0707 <jillfeel@gmail.com>
2026-03-15 23:30:48 +0800

    clear old command

--- sendmail.py
+++ sendmail.py
@@ -8,23 +8,24 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
-intents.members = True  # 建議開啟，以便找尋成員
-bot = commands.Bot(command_prefix="%", intents=intents)
+intents.members = True  
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 MY_GUILD_ID = 1477288273767301170
 
 
-
+@bot.tree.command(name="send_message",description="Send a message to everyone.")
+async def send_message(interaction: discord.Interaction,c :str):
+    await interaction.response.send_message(c)
 
 @bot.event
 async def on_ready():
     guild = discord.Object(id=MY_GUILD_ID)
     bot.tree.copy_global_to(guild=guild)
     await bot.tree.sync(guild=guild)
-    print("Bot is ready.")
 
-@bot.command
-async def send_message():
+
     
     
 
